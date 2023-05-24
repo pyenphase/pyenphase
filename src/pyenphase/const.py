9583a1fdc1d6070897fc6793c7f2dc8f13482bc2
Charles Garwood <cgarwood@gmail.com>
2023-05-24 14:30:52 -0400

    chore: add constants for API endpoint URLs (#5)
    chore: add constants for API endpoint URLs

---
+++ src/pyenphase/const.py
@@ -0,0 +1,17 @@
+# System Production
+URL_INVERTER_PRODUCTION = "/api/v1/production/inverters"
+URL_PRODUCTION = "/production.json"
+
+# Battery and Enpower Status
+URL_DRY_CONTACT_STATUS = "/ivp/ensemble/dry_contacts"
+URL_DRY_CONTACT_SETTINGS = "/ivp/ensemble/dry_contact_settings"
+URL_ENSEMBLE_INVENTORY = "/ivp/ensemble/inventory"
+URL_ENCHARGE_BATTERY = "/ivp/ensemble/power"
+URL_GRID_RELAY = "/ivp/ensemble/relay"
+URL_POWER_EXPORT = "/uvp/ss/pel_settings"
+URL_TARIFF = "/admin/lib/tariff"
+
+# Generator Configuration
+URL_GEN_CONFIG = "/ivp/ss/gen_config"
+URL_GEN_MODE = "/ivp/ss/gen_mode"
+URL_GEN_SCHEDULE = "/ivp/ss/gen_schedule"
