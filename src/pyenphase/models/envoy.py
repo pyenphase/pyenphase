6f787a90e4532266c1b8ff9fa784e3cf9aae60cb
J. Nick Koston <nick@koston.org>
2023-08-04 08:32:39 -1000

    cleanups

---
+++ src/pyenphase/models/envoy.py
@@ -0,0 +1,20 @@
+"""Model for an envoy."""
+
+from dataclasses import dataclass, field
+from typing import Any
+
+from .dry_contacts import EnvoyDryContact
+from .encharge import EnvoyEncharge
+from .enpower import EnvoyEnpower
+from .inverter import EnvoyInverter
+from .system_production import EnvoySystemProduction
+
+
+@dataclass(slots=True)
+class EnvoyData:
+    encharge: EnvoyEncharge | None
+    enpower: EnvoyEnpower | None
+    system_production: EnvoySystemProduction | None
+    dry_contacts: dict[str, EnvoyDryContact] = field(default_factory=dict)
+    inverters: dict[str, EnvoyInverter] = field(default_factory=dict)
+    raw: dict[str, dict[str, Any]] = field(default_factory=dict)
