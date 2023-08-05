b2093578d12978da49788ca08c3959d2c3fb3641
J. Nick Koston <nick@koston.org>
2023-08-05 07:43:36 -1000

    feat: export names at top level (#19)

--- src/pyenphase/__init__.py
+++ src/pyenphase/__init__.py
@@ -1,1 +1,23 @@
 """Python wrapper for Enphase Envoy API."""
+
+from .envoy import Envoy
+from .exceptions import (
+    EnvoyAuthenticationError,
+    EnvoyAuthenticationRequired,
+    EnvoyCommunicationError,
+    EnvoyError,
+    EnvoyFirmwareCheckError,
+    EnvoyFirmwareFatalCheckError,
+    EnvoyProbeFailed,
+)
+
+__all__ = (
+    "Envoy",
+    "EnvoyError",
+    "EnvoyCommunicationError",
+    "EnvoyFirmwareCheckError",
+    "EnvoyFirmwareFatalCheckError",
+    "EnvoyAuthenticationError",
+    "EnvoyAuthenticationRequired",
+    "EnvoyProbeFailed",
+)
