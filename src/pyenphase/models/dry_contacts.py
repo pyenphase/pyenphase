c26f5b4479666214793664ce0da5c87bf87a7e40
Charles Garwood <cgarwood@gmail.com>
2023-09-12 21:01:07 -0400

    fix: more dry contact settings should be optional

--- src/pyenphase/models/dry_contacts.py
+++ src/pyenphase/models/dry_contacts.py
@@ -53,16 +53,16 @@ class EnvoyDryContactSettings:
 
     id: str
     black_start: float | None
-    essential_end_time: float
-    essential_start_time: float
+    essential_end_time: float | None
+    essential_start_time: float | None
     generator_action: DryContactAction
     grid_action: DryContactAction
     load_name: str
-    manual_override: bool
+    manual_override: bool | None
     micro_grid_action: DryContactAction
     mode: DryContactMode
     override: bool
-    priority: float
+    priority: float | None
     pv_serial_nb: list[Any]
     soc_high: float
     soc_low: float
@@ -74,16 +74,16 @@ class EnvoyDryContactSettings:
         return cls(
             id=relay["id"],
             black_start=relay.get("black_s_start"),
-            essential_end_time=relay["essential_end_time"],
-            essential_start_time=relay["essential_start_time"],
+            essential_end_time=relay.get("essential_end_time"),
+            essential_start_time=relay.get("essential_start_time"),
             generator_action=DryContactAction(relay["gen_action"]),
             grid_action=DryContactAction(relay["grid_action"]),
             load_name=relay["load_name"],
-            manual_override=relay["manual_override"] == "true",
+            manual_override=relay.get("manual_override") == "true",
             micro_grid_action=DryContactAction(relay["micro_grid_action"]),
             mode=DryContactMode(relay["mode"]),
             override=relay["override"] == "true",
-            priority=relay["priority"],
+            priority=relay.get("priority"),
             pv_serial_nb=relay["pv_serial_nb"],
             soc_high=relay["soc_high"],
             soc_low=relay["soc_low"],
@@ -94,8 +94,6 @@ class EnvoyDryContactSettings:
         """Convert to API format."""
         retval = {
             "id": self.id,
-            "essential_end_time": self.essential_end_time,
-            "essential_start_time": self.essential_start_time,
             "gen_action": self.generator_action,
             "grid_action": self.grid_action,
             "load_name": self.load_name,
@@ -104,7 +102,6 @@ class EnvoyDryContactSettings:
             "micro_grid_action": self.micro_grid_action,
             "mode": self.mode,
             "override": str(self.override).lower(),
-            "priority": self.priority,
             "pv_serial_nb": self.pv_serial_nb,
             "soc_high": self.soc_high,
             "soc_low": self.soc_low,
@@ -113,5 +110,11 @@ class EnvoyDryContactSettings:
 
         if self.black_start is not None:
             retval["black_s_start"] = self.black_start
+        if self.essential_start_time is not None:
+            retval["essential_start_time"] = self.essential_start_time
+        if self.essential_end_time is not None:
+            retval["essential_end_time"] = self.essential_end_time
+        if self.priority is not None:
+            retval["priority"] = self.priority
 
         return retval
