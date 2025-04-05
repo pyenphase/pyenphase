ee6b9cd49fcffd136a49312eed9d15ecabe93e8d
Arie Catsman <catsmanac@outlook.com>
2025-04-05 17:53:39 +0200

    test: deduplicate fixture loading by using single function

--- tests/common.py
+++ tests/common.py
@@ -69,48 +69,148 @@ async def get_mock_envoy(update: bool = True):  # type: ignore[no-untyped-def]
 
 
 def prep_envoy(
-    version: str,
-    info: bool = False,  #: if true mock info
-    meters: bool = True,  #: if true mock meters
-    inverters: bool = True,  #: if true mock inverters
-) -> None:
-    """Setup response mocks for envoy requests."""
+    version: str,  #: name of version folder to read fixtures from
+) -> list[str]:
+    """Setup response mocks for envoy requests and return list of found mock files."""
     path = f"{_fixtures_dir()}/{version}"
     files = [f for f in listdir(path) if isfile(join(path, f))]
 
-    if info:
-        respx.get("/info").mock(
-            return_value=Response(200, text=load_fixture(version, "info"))
+    respx.get("/info").mock(
+        return_value=Response(200, text=load_fixture(version, "info"))
+    )
+    respx.get("/info.xml").mock(return_value=Response(200, text=""))
+
+    if "ivp_meters" in files:
+        try:
+            respx.get("/ivp/meters").mock(
+                return_value=Response(
+                    200, json=load_json_fixture(version, "ivp_meters")
+                )
+            )
+        except json.decoder.JSONDecodeError:
+            # v3 fw with html return 401
+            respx.get("ivp_meters").mock(return_value=Response(401))
+    else:
+        respx.get("/ivp/meters").mock(return_value=Response(404))
+
+    if "ivp_meters_readings" in files:
+        respx.get("/ivp/meters/readings").mock(
+            return_value=Response(
+                200, json=load_json_fixture(version, "ivp_meters_readings")
+            )
         )
+    else:
+        respx.get("/ivp/meters/readings").mock(return_value=Response(404))
 
-    if meters:
-        respx.get("/ivp/meters").mock(return_value=Response(200, text="[]"))
+    if "production" in files:
+        try:
+            json_data = load_json_fixture(version, "production")
+            respx.get("/production").mock(return_value=Response(200, json=json_data))
+        except json.decoder.JSONDecodeError:
+            # v3 fw reports production in html format
+            respx.get("/production").mock(
+                return_value=Response(200, text=load_fixture(version, "production"))
+            )
+    else:
+        respx.get("/production").mock(return_value=Response(404))
 
-    respx.get("/production").mock(
-        return_value=Response(200, text=load_fixture(version, "production"))
-    )
-    respx.get("/production.json").mock(
-        return_value=Response(200, text=load_fixture(version, "production.json"))
-    )
-    respx.get("/api/v1/production").mock(
-        return_value=Response(200, json=load_json_fixture(version, "api_v1_production"))
-    )
-    if inverters:
+    if "production.json" in files:
+        try:
+            respx.get("/production.json").mock(
+                return_value=Response(
+                    200, json=load_json_fixture(version, "production.json")
+                )
+            )
+            respx.get("/production.json?details=1").mock(
+                return_value=Response(
+                    200, json=load_json_fixture(version, "production.json")
+                )
+            )
+        except json.decoder.JSONDecodeError:
+            respx.get("/production.json").mock(return_value=Response(404))
+            respx.get("/production.json?details=1").mock(return_value=Response(404))
+    else:
+        respx.get("/production.json").mock(return_value=Response(404))
+        respx.get("/production.json?details=1").mock(return_value=Response(404))
+
+    if "api_v1_production" in files:
+        respx.get("/api/v1/production").mock(
+            return_value=Response(
+                200, json=load_json_fixture(version, "api_v1_production")
+            )
+        )
+    else:
+        respx.get("/api/v1/production").mock(return_value=Response(404))
+
+    if "api_v1_production_inverters" in files:
         respx.get("/api/v1/production/inverters").mock(
             return_value=Response(
                 200, json=load_json_fixture(version, "api_v1_production_inverters")
             )
         )
-    respx.get("/ivp/ensemble/inventory").mock(return_value=Response(200, json=[]))
+    else:
+        respx.get("/api/v1/production/inverters").mock(return_value=Response(404))
+
+    if "ivp_ensemble_inventory" in files:
+        respx.get("/ivp/ensemble/inventory").mock(
+            return_value=Response(
+                200, json=load_json_fixture(version, "ivp_ensemble_inventory")
+            )
+        )
+    else:
+        respx.get("/ivp/ensemble/inventory").mock(return_value=Response(404))
+
+    if "ivp_ensemble_dry_contacts" in files:
+        try:
+            json_data = load_json_fixture(version, "ivp_ensemble_dry_contacts")
+        except json.decoder.JSONDecodeError:
+            json_data = {}
+        respx.get("/ivp/ensemble/dry_contacts").mock(
+            return_value=Response(200, json=json_data)
+        )
+        respx.post("/ivp/ensemble/dry_contacts").mock(
+            return_value=Response(200, json=json_data)
+        )
+
+    if "ivp_ss_dry_contact_settings" in files:
+        try:
+            json_data = load_json_fixture(version, "ivp_ss_dry_contact_settings")
+        except json.decoder.JSONDecodeError:
+            json_data = {}
+        respx.get("/ivp/ss/dry_contact_settings").mock(
+            return_value=Response(200, json=json_data)
+        )
+        respx.post("/ivp/ss/dry_contact_settings").mock(
+            return_value=Response(200, json=json_data)
+        )
+
+    if "ivp_ensemble_power" in files:
+        try:
+            json_data = load_json_fixture(version, "ivp_ensemble_power")
+        except json.decoder.JSONDecodeError:
+            json_data = {}
+        respx.get("/ivp/ensemble/power").mock(
+            return_value=Response(200, json=json_data)
+        )
+
+    if "ivp_ensemble_secctrl" in files:
+        try:
+            json_data = load_json_fixture(version, "ivp_ensemble_secctrl")
+        except json.decoder.JSONDecodeError:
+            json_data = {}
+        respx.get("/ivp/ensemble/secctrl").mock(
+            return_value=Response(200, json=json_data)
+        )
 
     if "admin_lib_tariff" in files:
         try:
             json_data = load_json_fixture(version, "admin_lib_tariff")
         except json.decoder.JSONDecodeError:
-            json_data = None
+            json_data = {}
         respx.get("/admin/lib/tariff").mock(return_value=Response(200, json=json_data))
+        respx.put("/admin/lib/tariff").mock(return_value=Response(200, json=json_data))
     else:
-        respx.get("/admin/lib/tariff").mock(return_value=Response(401))
+        respx.get("/admin/lib/tariff").mock(return_value=Response(404))
 
     if "ivp_ss_gen_config" in files:
         try:
@@ -120,6 +220,8 @@ def prep_envoy(
         respx.get("/ivp/ss/gen_config").mock(return_value=Response(200, json=json_data))
     else:
         respx.get("/ivp/ss/gen_config").mock(return_value=Response(200, json={}))
+
+    return files
 
 
 def updater_features(updaters: list[EnvoyUpdater]) -> dict[str, SupportedFeatures]:
