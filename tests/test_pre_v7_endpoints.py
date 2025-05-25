0a64aa4b7b478a3538519de694099d7df47bf0df
J. Nick Koston <nick@koston.org>
2025-05-24 22:18:19 -0500

    cleanup

--- tests/test_pre_v7_endpoints.py
+++ tests/test_pre_v7_endpoints.py
@@ -481,7 +481,7 @@ async def test_with_3_7_0_firmware(
 
             try:
                 response = await self._probe_request(URL_PRODUCTION)
-                data = response.text
+                data = await response.text()
             except ENDPOINT_PROBE_EXCEPTIONS:
                 return None
             if "Since Installation" not in data:
@@ -492,7 +492,7 @@ async def test_with_3_7_0_firmware(
         async def update(self, envoy_data: EnvoyData) -> None:
             """Update the Envoy for this updater."""
             response = await self._request(URL_PRODUCTION)
-            production_data = response.text
+            production_data = await response.text()
             envoy_data.raw[URL_PRODUCTION] = production_data
             envoy_data.system_production = (
                 LegacyEnvoySystemProduction.from_production_legacy(production_data)
@@ -501,7 +501,7 @@ async def test_with_3_7_0_firmware(
     remove = register_updater(LegacyProductionScraper)
     assert LegacyProductionScraper in get_updaters()
     try:
-        envoy = await get_mock_envoy()
+        envoy = await get_mock_envoy(version, test_client_session)
         data = envoy.data
         assert data is not None
 
