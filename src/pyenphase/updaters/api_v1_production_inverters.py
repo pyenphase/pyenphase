0c090701d571b12dea4c65dd12f041c8c9833fbf
Alex Biddulph <abiddulph@4tel.com.au>
2025-05-31 16:15:19 +1000

    Fix commit hook violations

--- src/pyenphase/updaters/api_v1_production_inverters.py
+++ src/pyenphase/updaters/api_v1_production_inverters.py
@@ -1,7 +1,8 @@
 import logging
-from typing import Any
+from typing import Any, cast
 
-from ..const import URL_PRODUCTION_INVERTERS, SupportedFeatures
+from ..const import (URL_DEVICE_DATA, URL_PRODUCTION_INVERTERS,
+                     SupportedFeatures)
 from ..exceptions import ENDPOINT_PROBE_EXCEPTIONS, EnvoyAuthenticationRequired
 from ..models.envoy import EnvoyData
 from ..models.inverter import EnvoyInverter
@@ -17,31 +18,47 @@ class EnvoyApiV1ProductionInvertersUpdater(EnvoyUpdater):
         self, discovered_features: SupportedFeatures
     ) -> SupportedFeatures | None:
         """Probe the Envoy for this updater and return SupportedFeatures."""
-        try:
-            await self._json_probe_request(URL_PRODUCTION_INVERTERS)
-        except ENDPOINT_PROBE_EXCEPTIONS as e:
-            _LOGGER.debug(
-                "Production endpoint not found at %s: %s", URL_PRODUCTION_INVERTERS, e
-            )
-            return None
-        except EnvoyAuthenticationRequired as e:
-            _LOGGER.debug(
-                "Disabling inverters production endpoint as user does"
-                " not have access to %s: %s",
-                URL_PRODUCTION_INVERTERS,
-                e,
-            )
-            return None
-        self._supported_features |= SupportedFeatures.INVERTERS
-        return self._supported_features
+        for endpoint in (
+            URL_PRODUCTION_INVERTERS,
+            URL_DEVICE_DATA,
+        ):
+            try:
+                await self._json_probe_request(endpoint)
+                self._preferred_endpoint = endpoint
+                self._supported_features |= SupportedFeatures.INVERTERS
+                return self._supported_features
+            except ENDPOINT_PROBE_EXCEPTIONS as e:
+                _LOGGER.debug(
+                    "Production endpoint not found at %s: %s", endpoint, e
+                )
+            except EnvoyAuthenticationRequired as e:
+                _LOGGER.debug(
+                    "Disabling inverters production endpoint as user does"
+                    " not have access to %s: %s",
+                    endpoint,
+                    e,
+                )
+
+        _LOGGER.debug(
+            "Disabling inverters production endpoint as no valid endpoints found"
+        )
+        return None
 
     async def update(self, envoy_data: EnvoyData) -> None:
         """Update the Envoy for this updater."""
-        inverters_data: list[dict[str, Any]] = await self._json_request(
-            URL_PRODUCTION_INVERTERS
-        )
-        envoy_data.raw[URL_PRODUCTION_INVERTERS] = inverters_data
-        envoy_data.inverters = {
-            inverter["serialNumber"]: EnvoyInverter.from_v1_api(inverter)
-            for inverter in inverters_data
-        }
+        inverters_data: (
+            list[dict[str, Any]] | dict[str, Any]
+        ) = await self._json_request(self._preferred_endpoint)
+        envoy_data.raw[self._preferred_endpoint] = inverters_data
+        if self._preferred_endpoint == URL_PRODUCTION_INVERTERS:
+            envoy_data.inverters = {
+                inverter["serialNumber"]: EnvoyInverter.from_v1_api(inverter)
+                for inverter in cast(list[dict[str, Any]], inverters_data)
+            }
+        if self._preferred_endpoint == URL_DEVICE_DATA:
+            envoy_data.inverters = {
+                inverter["sn"]: EnvoyInverter.from_device_data(inverter)
+                for id, inverter in cast(dict[str, Any], inverters_data).items()
+                if id not in ("deviceCount", "deviceDataLimit")
+                and inverter["devName"] == "pcu"
+            }
