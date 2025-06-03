3cf3eddeaa147ede077e5717e04a2a79cec738f5
J. Nick Koston <nick@koston.org>
2025-06-03 21:22:03 +0100

    Revert "Update auth.py"
    This reverts commit afdcc2b69230ceb13959d7217c40cd2afb4cdae9.

--- src/pyenphase/auth.py
+++ src/pyenphase/auth.py
@@ -26,6 +26,7 @@ class CloseConnectionNotOKMiddleware:
         response = await handler(request)
 
         # If we get anything other than 200, close the connection
+            # Close the connection by closing the response
             # This ensures the connection is not reused
             response.close()
 
