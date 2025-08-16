b012b528a5fcfbee07e1b80d642fbd3a23eacdf2
Arie Catsman <catsmanac@outlook.com>
2025-08-16 21:59:58 +0200

    docs: add documentation to docstring and organize comments for docs

--- src/pyenphase/models/common.py
+++ src/pyenphase/models/common.py
@@ -8,18 +8,28 @@ from ..models.meters import CtType, EnvoyPhaseMode
 @dataclass(slots=True)
 class CommonProperties:
     """
-    Model for common properties of an envoy shared amongst all updaters.
+    Model for common properties for EnvoyUpdater class updaters.
 
-    One set are properties set during probe to share amongst updaters
-    and with clients. These should be reset at each probe run.
+    Class :any:`EnvoyUpdater` updaters each collect a specific data set
+    or information that can be provided by an Envoy. Depending on actual
+    make, firmware and installed components an actual Envoy will need
+    1 or more updater to provide all data for :any:`Envoy.update`.
 
-    More properties can be added, originators should handle reset as needed
-    by adding to reset_probe_properties to reset at probe or in a different way
-    or leave existing all lifetime.
+    Although each updater has its specific scope, some may need to share
+    information or make operational information available. One set of
+    properties are used during probe to share amongst updaters and with
+    client applications. These should be reset at each probe run. A second
+    set of properties are specific for an updater during its runtime. The
+    updater is in control of resetting the property as needed.
     """
 
     # probe properties here, also add to reset_probe_properties
     # shared amongst production updaters, needs reset before probe
+    #: Probe property, Fallback production endpoints for Metered without CT.
+    #: Solar production data has to be collected from different
+    #: sources in the Envoy, depending on actual Envoy configuration,
+    #: each utilizing a different updater. This list will be filled
+    #: with candidate updaters to use.
     production_fallback_list: list[str] = field(
         default_factory=list[str]
     )  #: Fallback production endpoints for Metered without CT
@@ -35,23 +45,25 @@ class CommonProperties:
     # other properties from here, reset by originator
 
     # controlled by meters updater
-    phase_count: int = 0  #: number of phases configured in envoy
-    ct_meter_count: int = 0  #: number of active ct meters
-    phase_mode: EnvoyPhaseMode | None = None  #: phase mode configured in the CT meters
-    consumption_meter_type: CtType | None = (
-        None  #: What type of consumption meter is installed, if installed
-    )
-    production_meter_type: CtType | None = (
-        None  #: What type of production meter is installed, if installed
-    )
-    storage_meter_type: CtType | None = (
-        None  #: What type of storage meter is installed, if installed
-    )
-    # controlled by production updater
-    active_phase_count: int = 0  #: number of phases actually reporting phase data
+    #: meters updater, number of phases configured in envoy
+    phase_count: int = 0
+    #: meters updater, number of active ct meters
+    ct_meter_count: int = 0
+    #: meters updater, phase mode configured in the CT meters
+    phase_mode: EnvoyPhaseMode | None = None
+    #: meters updater, what type of consumption meter is installed, if installed
+    consumption_meter_type: CtType | None = None
+    #: meters updater, what type of production meter is installed, if installed
+    production_meter_type: CtType | None = None
+    #: meters updater, what type of storage meter is installed, if installed
+    storage_meter_type: CtType | None = None
 
-    # controlled by
-    # none_probe_property: str = "hello world" #: test
+    # controlled by production updater
+    #: production updater, number of phases actually reporting phase data
+    active_phase_count: int = 0
+
+    # controlled by xyz updater
+    #: xyz updater, none_probe_property: str = "hello world" #: test
 
     def reset_probe_properties(self, is_metered: bool = False) -> None:
         """
--- src/pyenphase/updaters/base.py
+++ src/pyenphase/updaters/base.py
@@ -22,7 +22,24 @@ class EnvoyUpdater:
         request: Callable[[str], Awaitable[aiohttp.ClientResponse]],
         common_properties: CommonProperties,
     ) -> None:
-        """Initialize the Envoy endpoint."""
+        """
+        Base class for Envoy updaters.
+
+        Updaters should implement a subclass of EnvoyUpdater.
+
+        .. code-block:: python
+
+            class EnvoyXyzUpdater(EnvoyUpdater):
+
+        :param envoy_version: firmware version Envoy is running
+        :param probe_request: callable specified by
+            :any:`Envoy` to send probe request to the Envoy
+            during :any:`Envoy.probe`
+        :param request: callable specified by :any:`Envoy` to
+            send request to the Envoy during :any:`Envoy.update`
+        :param common_properties: properties to share between
+            probe and update or between updaters
+        """
         self._envoy_version = envoy_version
         self._probe_request = probe_request
         self._request = request
@@ -30,14 +47,44 @@ class EnvoyUpdater:
         self._common_properties = common_properties
 
     async def _json_request(self, end_point: str) -> Any:
-        """Make a request to the Envoy and return the JSON response."""
+        """
+        Make a request to the Envoy and return the JSON response.
+
+        Updaters should use this to collect data during :any:`update` method.
+
+        .. code-block:: python
+
+            xyz_json: dict[str, Any] = await self._json_request(
+                "/xyz/endpoint"
+            )
+
+        :param end_point: Envoy endpoint to request. See :any:`Envoy.request`
+        :raises EnvoyHTTPStatusError: If http status not in 2xx range
+        :raises Also See: :any:`Envoy.request`
+        :return: JSON content from response
+        """
         response = await self._request(end_point)
         if not (200 <= response.status < 300):
             raise EnvoyHTTPStatusError(response.status, str(response.url))
         return json_loads(end_point, await response.read())
 
     async def _json_probe_request(self, end_point: str) -> Any:
-        """Make a probe request to the Envoy and return the JSON response."""
+        """
+        Make a probe request to the Envoy and return the JSON response.
+
+        Updaters should use this to collect data during :any:`probe` method.
+
+        .. code-block:: python
+
+            xyz_json: dict[str, Any] = await self._json_probe_request(
+                "/xyz/endpoint"
+            )
+
+        :param end_point: Envoy endpoint to request. See :any:`Envoy.probe`
+        :raises EnvoyHTTPStatusError: If http status not in 2xx range
+        :raises Also See: :any:`Envoy.probe_request`
+        :return: JSON content from response
+        """
         response = await self._probe_request(end_point)
         if not (200 <= response.status < 300):
             raise EnvoyHTTPStatusError(response.status, str(response.url))
@@ -47,8 +94,52 @@ class EnvoyUpdater:
     async def probe(
         self, discovered_features: SupportedFeatures
     ) -> SupportedFeatures | None:
-        """Probe the Envoy for this updater and return SupportedFeatures."""
+        """
+        Probe the Envoy for this updater and return SupportedFeatures.
+
+        Updater must implement a probe method to report which features it supports.
+        Probe method of each registered updater is called by :any:'Envoy.probe'.
+        Intent of probe is to determine if this updater should be used
+        to obtain the data in its scope from the Envoy. If the Envoy
+        model does not provide the data the method should return None.
+
+        If the Envoy model does provide the data in scope, the method should
+        return a supported features mask for which it can provide data.
+        Any supported feature that is already included in the passed
+        discovered_features parameter should NOT be included in the result
+        as these are already provided by another updater and this updater
+        should back off from providing that feature data.
+
+        .. code-block:: python
+
+            async def probe(
+                    self, discovered_features: SupportedFeatures
+                ) -> SupportedFeatures | None:
+                    \"\"\"Probe the Envoy for this endpoint and return SupportedFeatures.\"\"\"
+                    pass
+
+        :param discovered_features: Mask of already discovered SupportedFeatures
+        :return: Mask of SupportedFeatures to be added to already discovered
+            features (and not yet in discovered_features).
+        """
 
     @abstractmethod
     async def update(self, envoy_data: EnvoyData) -> None:
-        """Update the Envoy for this updater."""
+        """
+        Get data from the Envoy and store in EnvoyData.
+
+        Updater must implement an update method to add/update data in
+        :any:`EnvoyData`. Update method of each registered updater is
+        called by :any:`Envoy.update`.
+
+        The update method is expected to obtain the required data from
+        the envoy, map it to the internal data model and store the data.
+
+        .. code-block:: python
+
+            async def update(self, envoy_data: EnvoyData) -> None:
+                \"\"\"Get data from the Envoy and store in EnvoyData.\"\"\"
+                pass
+
+        :param envoy_data: Envoy data model to store collected data in
+        """
